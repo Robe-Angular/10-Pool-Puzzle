3b7aa100124ed748c2a8dd6b24db942d75003ccb
robe <robe@robe.com>
2024-02-05 17:50:10 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_Pool_Puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Kangaroo Joey = new Kangaroo();
+            int Koala = Joey.Wombat(
+                Joey.Wombat(Joey.Wombat(1)));
+            try
+            {
+                Console.WriteLine((15 / Koala) + "eggs per pound");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("G'Day Mate!");
+            }
+        }
+    }
+}
---
+++ Kangaroo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _10_Pool_Puzzle
+{
+    class Kangaroo
+    {
+        FileStream fs;
+        int Croc;
+        int Dingo = 0;
+        public int Wombat(int Wallaby)
+        {
+            Dingo ++;
+            try
+            {
+                if(Wallaby > 0)
+                {
+                    fs = File.OpenWrite("wobbiegong");
+                    Croc = 0;
+                } else if (Wallaby < 0)
+                {
+                    Croc = 3;
+                }
+                else
+                {
+                    fs = File.OpenRead("wobbiegong");
+                    Croc = 1;
+                }
+            }
+            catch(IOException)
+            {
+                Croc = -3;
+            }
+            catch
+            {
+                Croc = 4;
+            }
+            finally
+            {
+                if(Dingo> 2)
+                {
+                    Croc -= Dingo;
+                }
+            }
+            Console.WriteLine(Croc);
+            return Croc;
+        }
+    }
+}
